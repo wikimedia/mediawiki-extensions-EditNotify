f6e19a6bb5f6c2a62d8cd9ab5fa0499b77864b09
Paladox <thomasmulhall410@yahoo.com>
2021-02-18 17:51:16 +0000

    Fix "PHP Notice: Undefined variable: fieldValueCategoryUserArray"
    Change-Id: Ib4c467ee86873c82b6d8b9dc760acda53606cddd
    (cherry picked from commit 7ee6e4eb2dec64d5808146d20a80c32d272a1f04)

--- EditNotify.hooks.php
+++ EditNotify.hooks.php
@@ -485,7 +485,7 @@ class EditNotifyHooks {
 								self::templateFieldValueNotify( $title, 'edit-notify-template-value-category', $fieldValueCategoryUser,
 									$changedFieldName, $changedFieldValue, $template, $existingField[$changedFieldName], $fieldValueCategory );
 							}
-							unset( $fieldValueCategoryUserArray );
+							$fieldValueCategoryUserArray = [];
 						}
 					}
 				}
