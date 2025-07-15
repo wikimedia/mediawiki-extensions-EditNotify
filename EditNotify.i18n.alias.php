c33f269973886c7187f3a894eb92ad35837bf887
anterdc99 <xuminruixuan@live.cn>
2025-07-15 13:21:25 +0800

    EditNotify: Translate special page aliases into Chinese
    Change-Id: I5b6d3c5970a3de69f2d8d48e6bbd01ce15a8399e

--- EditNotify.i18n.alias.php
+++ EditNotify.i18n.alias.php
@@ -12,3 +12,18 @@ $specialPageAliases = [];
 $specialPageAliases['en'] = [
 	'EditNotify' => [ 'EditNotify' ],
 ];
+
+/** Chinese (中文) */
+$specialPageAliases['zh'] = [
+	'EditNotify' => [ 'EditNotify' ],
+];
+
+/** Simplified Chinese (中文（简体）) */
+$specialPageAliases['zh-hans'] = [
+	'EditNotify' => [ '编辑通知' ],
+];
+
+/** Traditional Chinese (中文（繁體）) */
+$specialPageAliases['zh-hant'] = [
+	'EditNotify' => [ '編輯通知' ],
+];
